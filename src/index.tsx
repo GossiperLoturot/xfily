14d92229f165493599e0d8d5255b803cc55daa7b
GossiperLoturot <ts20030211@gmail.com>
2023-04-20 13:22:00 +0900

    yewによる実装を巻き戻しsolidjsに変更
    Revert "画像変換ページを別ファイルに分離"
    
    This reverts commit 95e4fc109babdbf5561413cc9490857b363ed720.
    
    Revert "ページデザインの微調整"
    
    This reverts commit 979ccf6931048abb300fa51022a7bc9257fcdd59.
    
    Revert "ヘッダーを追加"
    
    This reverts commit 4b4dca820655e7cd26b1f3cbd54c5f54cc6cb9ea.
    
    Revert "static.ymlの修正"
    
    This reverts commit a1d4955ee126afd98366bfd70c2e0f67a32872ac.
    
    Revert "static.ymlを修正"
    
    This reverts commit bbc5ccefdef2d83ccb0c0f50d270b41a5607059f.
    
    Revert "static.ymlを修正"
    
    This reverts commit 4abacd4ea306f26f64edb5d600c47e0b2c803f4d.
    
    Revert "GitHub PagesへのデプロイCI/CDを追加"
    
    This reverts commit 7a98e0da0910b2b69f09a9f762aa11931465bd81.
    
    Revert "画像変換のページを追加"
    
    This reverts commit 7ef83bdc9a18430046fcb78b1a7d539f585b91f1.
    
    Revert "フレームワークをyewに変更"
    
    This reverts commit 9bd187d105b00cbeb6e687b175ba9e1237edf449.

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+/* @refresh reload */
+import "./index.css";
+import { render } from "solid-js/web";
+
+import App from "./pages/App";
+
+render(() => <App />, document.getElementById("root") as HTMLElement);
